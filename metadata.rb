7e4bf3efb9ee292aba818896c69f78f6813440df
Daniel Givens <daniel.givens@rackspace.com>
2014-10-31 14:19:51 -0500

    Version bump to 2.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures heat-iptables'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
